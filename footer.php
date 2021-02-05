c249dae92c94441e697a1219c0020ab5bba5d783
ushergodwin <65310616+ushergodwin@users.noreply.github.com>
2021-02-05 14:36:58 +0300

    added copyright and a javascript function

--- footer.php
+++ footer.php
@@ -1,7 +1,27 @@
+<div class="container-fluid mt-3">
+    <div class="row justify-content-center">
+        <span class="text-muted">Copyright &copy; Toursim Center <?= date('Y')?></span>
+    </div>
+</div>
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
     <script src="js/tourism.js"></script>
+    <script>
+        function confAction(action) {
+            var conf = false;
+            if (action === "approve") {
+                conf = confirm("Approve this order?");
+            }else if(action === "cancel") {
+                conf = confirm("Cancel Approval for this order?");
+            }else if(action === "decline") {
+                conf = confirm("Decline this order? \n Action can not be undone!");
+            }else {
+                conf = false;
+            }
+            return conf;
+        }
+    </script>
 </body> 
     </body> 
 </html>
