baf9b9e923cbaab75c6b5d7b70f076ab625e0303
ushergodwin <65310616+ushergodwin@users.noreply.github.com>
2021-02-05 16:13:34 +0300

    changed the UI

--- dashboard.php
+++ dashboard.php
@@ -3,50 +3,31 @@
 <main class="container-fluid">
     <div class="row">
         <div class="col-md-4 col-lg-4 col-xl-4">
-            <div class="card mt-3 bg-warning py-1">
-                <div class="card-header py-1">
-                <h5 class="font-weight-bold"> Pending orders </h5>
-                </div>
-                <div class="card-body">
-                    <div class="row">
-                       <span class="ds_number"> <?php echo $pending ?> </span>
-                     
+                <div class="card card-body shadow mt-3 bg-warning">
+                    <div class="row justify-content-center">
+                    <h5 class="font-weight-bold"> Pending orders <span class="badge badge-light ds_number"> <?php echo $pending ?> </span></h5>
                     </div>
                 </div>
+        </div>
+
+        <div class="col-md-4 col-lg-4 col-xl-4">
+                <div class="card card-body shadow mt-3 bg-success">
+                    <div class="row justify-content-center">
+                    <h5 class="font-weight-bold"> Approved orders <span class="badge badge-light ds_number"> <?php echo $complete ?> </span></h5>
+                    </div>
             </div>
         </div>
 
         <div class="col-md-4 col-lg-4 col-xl-4">
-            <div class="card mt-3 bg-success py-1">
-                <div class="card-header py-1">
-                <h5 class="font-weight-bold"> Approved orders </h5>
-                </div>
-                <div class="card-body">
-                    <div class="row">
-                       <span class="ds_number"> <?php echo $complete ?> </span>
+                <div class="card card-body shadow mt-3 bg-danger">
+                    <div class="row justify-content-center">
+                        <h5 class="font-weight-bold"> Declined orders <span class="badge badge-light ds_number"> <?php echo $declined ?> </span></h5>
                      
                     </div>
                 </div>
-            </div>
-        </div>
-
-        <div class="col-md-4 col-lg-4 col-xl-4">
-            <div class="card mt-3 bg-danger py-1">
-                <div class="card-header py-1">
-                <h5 class="font-weight-bold"> Declined orders </h5>
-                </div>
-                <div class="card-body">
-                    <div class="row">
-                       <span class="ds_number"> <?php echo $declined ?> </span>
-                     
-                    </div>
-                </div>
-            </div>
         </div>
     </div>
 </main>
 
  
-<?php
-include 'footer.php';
- ?>
+<?php include 'footer.php'; ?>
