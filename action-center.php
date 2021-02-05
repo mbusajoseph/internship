7b72c0650bed70b7119e2c9f427a78fd6fa99cb6
ushergodwin <65310616+ushergodwin@users.noreply.github.com>
2021-02-05 14:29:18 +0300

    added header.php
    Added header.php, ability to update the package description

--- action-center.php
+++ action-center.php
@@ -1,57 +1,4 @@
-<?php include 'app_view.php' ?>
-<!DOCTYPE html>
-<html> 
-    <head>  
-    <link rel="icon" href="" size="16x16" type=""/>
-        <title>Dashboard | TOURISM CENTER</title>  
-        <!-- <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
-        <link rel="stylesheet" type="text/css" href="style.css">-->
-       <!-- <link rel="stylesheet" type="text/css" href="assets/fontawesome/css/all.min.css">
-        <script type="text/javascript" src="assets/js/jquery-3.5.1.min.js"></script> -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
-        <link rel="stylesheet" href="css/style.css">
-    </head> 
-    <body>  
-       <nav class="navbar navbar-expand-md navbar-light bg-light">
-    <a href="dahsboard.php" class="navbar-brand">TOURISM CENTER</a>
-    <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
-        <div class="navbar-nav">
-            <a href="#" class="nav-item nav-link active"></a>
-            <a href="./dashboard.php" class="nav-item nav-link">HOME</a>
-                <a href="#package" class="nav-item nav-link">Add Package</a>
-            <div class="nav-item dropdown">
-            <a href="#" class="nav-item nav-link">All Package <span class="badge badge-dark"><?=$num_packs?></span> </a>
-            </div>
-              <div class="nav-item dropdown">
-                <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">NATIONAL_PARKS</a>
-                <div class="dropdown-menu">
-                   <?php foreach ($parks as $park): ?>
-                    <a href="#" class="dropdown-item"><?= $park['name'] ?></a>
-                   <?php endforeach ?>
-                </div>
-            </div>
-        </div>
-        <form class="form-inline" action="search.php" method="get">
-            <div class="input-group">                    
-                <input type="search" class="form-control" placeholder="Search" name="q">
-                <div class="input-group-append">
-                    <button type="submit" class="btn btn-secondary"><i class="fas fa-search"></i></button>
-                </div>
-            </div>
-        </form>
-        <div class="navbar-nav">
-           <a href="javascript:void(0)" class="nav-item nav-link">      
-       <strong class="text-primary"><i class="fas fa-user"></i> Hi, <?= $username?>
-     </strong>
-            <a href="auth.php?logout='1'" class="nav-item nav-link" onclick="return confirm('Logout?')">Logout</a>
-        </div>
-    </div>
-</nav>
+<?php include 'app_view.php'; include 'header.php'; ?>
 <main class="container-fluid">
     <div class="row">
         <div class="col-md-12 col-lg-12 col-xl-12">
@@ -76,6 +23,7 @@
                                     <th>PACKAGE ID</th>
                                     <th>PACKAGE NAME</th>
                                     <th>PACKAGE COST</th>
+                                    <th>DESCRIPTION</th>
                                     <th><div class="before-1 d-none">
                                     <span class="spinner-border spinner-border-sm text-success"></span> saving...
                                     </div></th>
@@ -89,6 +37,7 @@
                                         <input type="hidden" name="id" value="<?= $pk_id ?>"/>
                                          <td><input type="text" name="name" class="edit-input" value="<?= $pk_name ?>"/></td>
                                         <td><input type="text" name="price" class="edit-input" value="<?= $pk_cost ?>"/></td>
+                                        <th><textarea name="desc" rows="5" class="form-control"><?= $pk_desc?></textarea></th>
                                         <td><button type="submit" class="btn btn-success btn-sm edit-btn">Save Changes</button> </td>
                                     </form>
                                 </tr>
