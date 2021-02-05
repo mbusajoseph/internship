2450c8ba831d0aa96b1d2e1682a747b3847050ac
ushergodwin <65310616+ushergodwin@users.noreply.github.com>
2021-02-05 14:40:22 +0300

    updated app_view

--- app_view.php
+++ app_view.php
@@ -10,7 +10,7 @@ $data = Tourism::get_services();
 //get the national parks
 $parks = Tourism::get_national_parks();
 //get packages
-$packages = Tourism::get_packages();
+//$packages = Tourism::get_packages();
 //capture the approval action
 $response = "";
 if (isset($_REQUEST['approve'])) {
@@ -20,13 +20,32 @@ if (isset($_REQUEST['approve'])) {
 if (isset($_REQUEST['cancel'])) {
     $response = Tourism::cancel_approval();
 }
+//decline order
+if (isset($_REQUEST['decline'])) {
+    $response = Tourism::decline_order();
+}
 //get the requset to edit a package
-$pk_id = $pk_name = $pk_cost = "";
+$pk_id = $pk_name = $pk_cost = $pk_desc = "";
 if (isset($_REQUEST['action'])) {
     Tourism::get_package_by_id();
     $pk_id = Tourism::$packageId;
     $pk_name = Tourism::$packageName;
     $pk_cost = Tourism::$packageCost;
+    $pk_desc = Tourism::$packageDesc;
 }
 //get number of packages
 $num_packs = Tourism::num_of_packages();
+//get orders
+$orders = Tourism::get_orders();
+//get services
+$services = Tourism::get_national_parks();
+//get service by id
+if (isset($_REQUEST['data'])) {
+    $service_id = strip_tags($_GET['data']);
+    $services_by_id = Tourism::get_packages($service_id);
+    $result = Tourism::get_service($service_id);
+}
+//count the orders by status
+$pending = Tourism::order_status("pending");
+$complete = Tourism::order_status("approved");
+$declined = Tourism::order_status("declined");
